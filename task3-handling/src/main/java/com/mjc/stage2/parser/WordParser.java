c528efd33ee1fbe0027a72107da86ad4289b1f62
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-03-26 15:25:23 +0600

    Update WordParser.java

--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -1,8 +1,12 @@
 package com.mjc.stage2.parser;
 
 
-public class WordParser {
+import com.mjc.stage2.entity.AbstractTextComponent;
 
+public class WordParser extends AbstractTextParser {
     // Write your code here!
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
 
+    }
 }
