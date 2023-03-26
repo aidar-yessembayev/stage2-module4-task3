5f6277e101e0740b85c7325ba2a2ba7761f9857d
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-03-26 15:24:06 +0600

    Update SymbolLeaf.java

--- task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
+++ task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
@@ -1,8 +1,32 @@
 package com.mjc.stage2.entity;
 
-public class SymbolLeaf {
+import com.mjc.stage2.exception.HandlingException;
 
+public class SymbolLeaf extends AbstractTextComponent {
     private char value;
 
     // Write your code here!
+    public SymbolLeaf(TextComponentType componentType) {
+        super(componentType);
+    }
+
+    @Override
+    public String operation() {
+        return null;
+    }
+
+    @Override
+    public void add(AbstractTextComponent textComponent) throws HandlingException {
+
+    }
+
+    @Override
+    public void remove(AbstractTextComponent textComponent) throws HandlingException {
+
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
 }
