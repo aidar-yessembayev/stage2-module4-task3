f366a6319f470b15d8024176369de5b87ced48e4
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-03-26 14:57:48 +0600

    Update RectangleObserver.java

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,18 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.event.RectangleEvent;
+
+public class RectangleObserver implements Observer {
     // Write your code here!
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        Rectangle rectangle = event.getSource();
+
+        int id = rectangle.getId();
+        double sideA = rectangle.getSideA();
+        double sideB = rectangle.getSideB();
+
+    }
 }
